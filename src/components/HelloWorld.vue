927347df8a05be54e5a77ce23d4bfc687400629e
zhanghj <1058776714@qq.com>
2019-10-29 14:58:23 +0800

    第三次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="main">
+      <div class="keyword">
+        <input type="text" />
+      </div>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  created() {
+    console.log("xx");
+    axios
+      .get("/api/airport/search", {
+        params: {
+          keyword: "guangzhou"
+        }
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
