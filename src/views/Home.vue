002dceeabff7eb4ee6cb4bd1fafcb0cc91d7be7c
zhanghj <1058776714@qq.com>
2019-10-29 15:33:48 +0800

    第七次提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Hello Jenkins2" />
+    <HelloWorld msg="Hello Jenkins3" />
   </div>
 </template>
 
