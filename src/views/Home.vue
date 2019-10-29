4414867172db0490f0c1df2f6358fb245545394f
zhanghj <1058776714@qq.com>
2019-10-29 15:20:37 +0800

    第四次提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Hello Docker" />
+    <HelloWorld msg="Hello Jenkins" />
   </div>
 </template>
 
